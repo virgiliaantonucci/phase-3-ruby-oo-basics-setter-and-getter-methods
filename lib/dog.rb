fe93308df4c53bea6196267ab380a9653f7cd69b
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-07-04 15:49:58 -0400

    commit tree b2203c4bee97b7aebf5c99e8c791f1327b5b11fb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
-end
+    def name
+      @name
+    end
+  
+    def name=(name)
+      @name = name
+    end
+  
+    def breed
+      @breed
+    end
+  
+    def breed=(breed)
+      @breed = breed
+    end
+  end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
-
-end
+    def name
+      @name
+    end
+  
+    def name=(name)
+      @name = name
+    end
+  
+    def job
+      @job
+    end
+  
+    def job=(job)
+      @job = job
+    end
+  end
